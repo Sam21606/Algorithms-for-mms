f89adaddf61e47d9772aa51328880beaa7c4eae0
Sam21606 <Sam21606@users.noreply.github.com>
2025-09-30 15:39:21 +0200

    prevering-related-nodes

--- Astar Simple/main.py
+++ Astar Simple/main.py
@@ -238,8 +238,8 @@ def determinFastetNode(currentNodes: List[Helper.Node], currentNode : Helper.Nod
         if node.cost <= currentFastest.cost and node is not currentNode:
             if currentFastest.cost != node.cost:
                 currentFastest = node
-            # elif node.parent == currentNode:
-            #      currentFastest = node
+            elif node.parent == currentNode:
+                currentFastest = node
     return currentFastest
 
 def calculateHeuristic(currentNode: Helper.Node) -> float:
--- Djikstra/main.py
+++ Djikstra/main.py
@@ -195,8 +195,8 @@ def determinFastetNode(currentNodes: List[Helper.Node], currentNode : Helper.Nod
         if node.cost <= currentFastest.cost and node is not currentNode:
             if currentFastest.cost != node.cost:
                 currentFastest = node
-            # elif node.parent == currentNode:
-            #     currentFastest = node
+            elif node.parent == currentNode:
+                currentFastest = node
     return currentFastest
 
 def moveToNewNode(currentNode : Helper.Node, newNode : Helper.Node, direction : int)-> int:
